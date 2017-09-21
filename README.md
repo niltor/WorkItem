0d8f4e9b963e54cc5461b76eee3ddae7ab33984f
NilTor <zpty@outlook.com>
2017-09-21 19:46:43 +0800

    rebuild

--- README.md
+++
@@ -1,2 +0,0 @@
-# WorkItem
-workItem for vsts
--- app/src/main/java/msdev/cc/workitem/MainActivity.java
+++ app/src/main/java/msdev/cc/workitem/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener{
