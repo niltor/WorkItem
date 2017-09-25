810d297479862b3f7b369d18ddac1f868ac31ec1
NilTor <zpty@outlook.com>
2017-09-25 23:59:48 +0800

    change to xamarin

---
+++ VSTS/MainActivity.cs
@@ -0,0 +1,19 @@
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace VSTS
+{
+    [Activity(Label = "VSTS", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
