6bea852c83054a2269dcccb1d6150835bc9f0c01
NilTor <zpty@outlook.com>
2017-09-29 15:05:49 +0800

    test bottomNavigation

--- VSTS/MainActivity.cs
+++ VSTS/MainActivity.cs
@@ -13,37 +13,28 @@ using Android.Util;
 using VSTS.Services;
 using System.IO;
 using Android.Content.Res;
+using Android.Support.V7.App;
 
 namespace VSTS
 {
     [Activity(Label = "MainActivity")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
-        private TextView messageTV;
-        private TextView tokenTV;
-        private Button button;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
 
-            messageTV = (TextView)FindViewById(Resource.Id.message);
-            tokenTV = (TextView)FindViewById(Resource.Id.token);
-            button = (Button)FindViewById(Resource.Id.button);
+
 
             var sp = GetSharedPreferences("config", FileCreationMode.Private);
             var token = sp.GetString("token", string.Empty);
 
-            tokenTV.Text = token;
 
             var service = new TokenService(this);
-            button.Click += async delegate
-             {
-                 await service.GetMyProjectAsync(token);
-             };
+ 
 
-            // Create your application here
         }
     }
 }
