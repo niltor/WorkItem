56d08ccb4c38fa215186a92cbad669fce3f59240
NilTor <zpty@outlook.com>
2017-09-29 19:56:27 +0800

    debug bottom navigation

---
+++ VSTS/Fragments/HomeFragment.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace VSTS.Fragments
+{
+    public class HomeFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.HomeFragment, null);
+
+        }
+
+        internal static HomeFragment NewInstance()
+        {
+            return new  HomeFragment{
+                Arguments = new Bundle()
+            };
+        }
+    }
+}
--- VSTS/MainActivity.cs
+++ VSTS/MainActivity.cs
@@ -1,40 +1,58 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.Util;
 using VSTS.Services;
-using System.IO;
-using Android.Content.Res;
 using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using VSTS.Fragments;
+using Android.App;
 
 namespace VSTS
 {
     [Activity(Label = "MainActivity")]
     public class MainActivity : AppCompatActivity
     {
+        private Fragment fragment;
+        private BottomNavigationView bottomNavigation;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
 
+            bottomNavigation = (BottomNavigationView)FindViewById(Resource.Id.bottom_navigation);
 
+            bottomNavigation.NavigationItemSelected += BottomNavigation_NavigationItemSelected;
 
             var sp = GetSharedPreferences("config", FileCreationMode.Private);
             var token = sp.GetString("token", string.Empty);
 
 
             var service = new TokenService(this);
- 
 
+        }
+
+        private void BottomNavigation_NavigationItemSelected(object sender, BottomNavigationView.NavigationItemSelectedEventArgs e)
+        {
+            switch (e.Item.ItemId)
+            {
+                case Resource.Id.menu_home:
+                    fragment = HomeFragment.NewInstance();
+                    break;
+                //case Resource.Id.menu_audio:
+                //    fragment = Fragment2.NewInstance();
+                //    break;
+                //case Resource.Id.menu_video:
+                //    fragment = Fragment3.NewInstance();
+                //    break;
+            }
+
+            if (fragment == null)
+                return;
+
+            FragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content_frame, fragment)
+                .Commit();
         }
     }
 }
