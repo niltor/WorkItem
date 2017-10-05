a964243cbf5a5575bf53d7c39bb58a804ba2c959
NilTor <zpty@outlook.com>
2017-10-05 17:20:10 +0800

    add  floatActionButton

--- VSTS/Fragments/WorkItemFragment.cs
+++ VSTS/Fragments/WorkItemFragment.cs
@@ -1,3 +1,4 @@
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
@@ -31,18 +32,35 @@ namespace VSTS.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
 
-            var rootView = inflater.Inflate(Resource.Layout.WorkItemFragment, container, false);
+            var rootView = inflater.Inflate(Resource.Layout.WorkItemFragment, container,false);
             var tabLayout = rootView.FindViewById<TabLayout>(Resource.Id.workitem_tablayout);
             var viewPager = rootView.FindViewById<ViewPager>(Resource.Id.workitem_viewpager);
+            var floatButton = rootView.FindViewById<FloatingActionButton>(Resource.Id.workitem_float_action_button);
 
+            floatButton.Click += FloatButton_Click;
             viewPager.Adapter = new MyAdapter(ChildFragmentManager);
             viewPager.OffscreenPageLimit = 3;
             tabLayout.SetupWithViewPager(viewPager);
-
             return rootView;
         }
 
+        private void FloatButton_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(Activity, typeof(AddWorkitemActivity));
+            Activity.StartActivity(intent);
+        }
 
+        public void OnCheckedChanged(FloatingActionButton fabView, bool isChecked)
+        {
+            switch (fabView.Id)
+            {
+                case Resource.Id.workitem_float_action_button:
+                    Log.Debug("vsts", string.Format("FAB 1 was {0}.", isChecked ? "checked" : "unchecked"));
+                    break;
+                default:
+                    break;
+            }
+        }
 
         public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
         {
